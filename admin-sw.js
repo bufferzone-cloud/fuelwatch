82af6f0382ab7b7e2b55f7d9ee143e0ace23f743
bufferzone-cloud <jubelmobz19@gmail.com>
2026-04-15 13:43:26 +0200

    Create admin-sw.js

---
+++ admin-sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'fuelradar-admin-cache-v1';
+
+const urlsToCache = [
+  '/fuelwatch/admin.html',
+  '/fuelwatch/admin-manifest.json',
+  '/fuelwatch/logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js',
+  'https://upload-widget.cloudinary.com/global/all.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
